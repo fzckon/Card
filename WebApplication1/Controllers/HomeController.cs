2e2a442a2649a2be4106c2b0586da4b38b3dce37
Chengkai Zhang <v-chenzh@microsoft.com>
2018-08-31 17:36:58 +0800

    nlog

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -5,13 +5,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using NLog;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        //public static Logger logger = LogManager.GetCurrentClassLogger();
+        public static Logger logger1 = LogManager.GetLogger("Trace");
+        public static Logger logger2 = LogManager.GetLogger("aaa");
         public IActionResult Index()
         {
+            var a = LogManager.DefaultCultureInfo;
+            var c = LogManager.LogFactory;
+            logger1.Warn("Test1-Warn");
+            logger1.Trace("Test1-Trace");
+            logger1.Info("Test1-Info");
+            logger1.Error("Test1-Error");
+            logger1.Debug("Test1-Debug");
+            logger1.Fatal("Test1-Fatal");
+
+            logger2.Warn("Test2-Warn");
+            logger2.Trace("Test2-Trace");
+            logger2.Info("Test2-Info");
+            logger2.Error("Test2-Error");
+            logger2.Debug("Test2-Debug");
+            logger2.Fatal("Test2-Fatal");
+
+
+            //logger.Warn("Test-Warn");
+            //logger.Trace("Test-Trace");
+            //logger.Info("Test-Info");
+            //logger.Error("Test-Error");
+            //logger.Debug("Test-Debug");
+            //logger.Fatal("Test-Fatal");
+
             return View();
         }
 
